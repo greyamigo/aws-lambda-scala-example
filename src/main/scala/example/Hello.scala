2a40d20ec0f5b8d9f9ec519de1460d3c9db0a393
Georgy Babu Abraham <georgy.abraham@news.com.au>
2018-07-06 10:15:04 +1000

    Tidy up , Functionify

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -4,6 +4,7 @@ import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.services.lambda.model.{InvokeRequest, InvokeResult}
 import com.amazonaws.services.lambda.{AWSLambda, AWSLambdaClientBuilder}
 import java.nio.ByteBuffer
+import java.nio.charset.Charset
 
 object Hello extends App {
 
@@ -12,24 +13,38 @@ object Hello extends App {
     .withCredentials(credentials)
     .withRegion("ap-southeast-2")
     .build()
-  val invokeRequest = new InvokeRequest
-  invokeRequest.setPayload("{\"postcode\":\"2140\"}")
-  invokeRequest.setFunctionName("postcodeWeatherZone-uat-index")
-  val res: InvokeResult = lambdaClient.invoke(invokeRequest)
-  import java.nio.charset.Charset
 
-  def byteBufferToString(buffer:ByteBuffer, charset: Charset) = {
+  def byteBufferToString(buffer:ByteBuffer, charset: Charset = Charset.forName("UTF-8")) = {
     var bytes =  Array[Byte]()
-    if (buffer.hasArray) { bytes = buffer.array }
+    if (buffer.hasArray) {
+      bytes = buffer.array
+    }
     else {
       val bytes = new Array[Byte](buffer.remaining)
       buffer.get(bytes)
     }
     new String(bytes, charset)
   }
-  println(byteBufferToString(
-    lambdaClient.invoke(invokeRequest).getPayload(),
-    Charset.forName("UTF-8")));
-  println(res.toString)
+
+  def createRequest(payload: String, functionName: String) ={
+    new InvokeRequest().withFunctionName(functionName).withPayload(payload)
+  }
+
+  def getPayload(postcode:Int) =
+    s""" {
+      "query": {
+        "postcode": ${postcode}
+      }
+    }"""
+  def getJSONResponseForLambdaInvoke(postcode: Int, functionName: String) : Option[String] = {
+    val invokeRequest= createRequest(getPayload(postcode), functionName)
+    val res: InvokeResult = lambdaClient.invoke(invokeRequest)
+    res.getFunctionError match{
+      case null =>     Some(byteBufferToString(res.getPayload))
+      case e =>        println(e) ; None
+    }
+  }
+  println(getJSONResponseForLambdaInvoke(2140,"postcodeMapping-uat-getEntry"))
+  println(getJSONResponseForLambdaInvoke(2140,"postcodeWeatherZone-uat-index"))
 
 }
